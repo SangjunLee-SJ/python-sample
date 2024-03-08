6355af2ae06e2a62c6ba8ab921df62036ef63492
Sangjunlee-SJ <sangjunlee4github@gmail.com>
2024-03-08 18:42:43 +0900

    updates

--- 10.Show dates by weekday.py
+++ 10.Show dates by weekday.py
@@ -8,17 +8,14 @@ def display_weeks(start_date, end_date):
     week_number = 1
 
     while current_date <= end_date:
-        print(f"{week_number}주차-{current_date.strftime('%Y.%m.%d')}")
+        print(f"{week_number} Week-{current_date.strftime('%Y.%m.%d')}")
         current_date += timedelta(days=7)
         week_number += 1
 
-# Start date와 End date를 입력 받기
-start_date_str = input("Start date를 입력하세요 (예: 2024.03.08): ")
-end_date_str = input("End date를 입력하세요 (예: 2024.06.14): ")
+start_date_str = input("Input Start date (ex: 2024.03.08): ")
+end_date_str = input("Input End date (ex: 2024.06.14): ")
 
-# 문자열을 datetime 객체로 변환
 start_date = datetime.strptime(start_date_str, "%Y.%m.%d")
 end_date = datetime.strptime(end_date_str, "%Y.%m.%d")
 
-# 주차 텍스트 표시
 display_weeks(start_date, end_date)
